a29ec690e462cb20b83281fb077520dfa1c840f8
TanujKumar41 <tanujkumar191001@gmail.com>
2024-09-26 12:47:02 +0530

    alignment changes

--- src/app/Hero/hero5/page.tsx
+++ src/app/Hero/hero5/page.tsx
@@ -4,7 +4,7 @@ import './Hero5.scss'; // Import your SCSS file here
 const Hero5 = () => {
     return (
         <div className="hero5-container" style={{ padding: '5em 0 7em 0', flexDirection: 'column', justifyContent: 'space-between' }}>
-            <div className="hero5-content" style={{ maxWidth: '600px', padding: '2em' }}>
+            <div className="hero5-content" style={{ maxWidth: '1500px', padding: '2em' }}>
                 <h6 className="hero5-title" style={{ textAlign: 'center' }}>What we offer</h6>
                 <h2 className="hero5-subtitle" style={{ textAlign: 'center' }}>In every chess position, there are opportunities.</h2>
             </div>
@@ -16,15 +16,13 @@ const Hero5 = () => {
                         <div className="icon-box-title" style={{ marginBottom: '7px' ,color:'white'}}>Adult</div>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
                     </div>
-                </div>
-                {/* Box 2 */}
-                <div className="hero5-box" style={{ width: '33%', textAlign: 'left', padding: '2em' }}>
                     <div className="icon-box" style={{ border: '1px solid #ccc', padding: '2em', borderRadius: '5px',backgroundColor:'black',color:'white'  }}>
                         <i className="fas fa-chess-bishop" style={{ fontSize: '28px', marginBottom: '7px',backgroundColor:'white',color:'black' }}></i>
                         <div className="icon-box-title" style={{ marginBottom: '7px' }}>Private</div>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
                     </div>
                 </div>
+                {/* Box 2 */}
                 {/* Call to Action Box 1 */}
                 <div className="hero5-cta" style={{ width: '33%', textAlign: 'left', padding: '2em' }}>
                     <div className="cta-box" style={{ backgroundImage: 'url(http://kit.creativemox.com/gambit/wp-content/uploads/sites/10/2023/10/school-boys-playing-chess-e1698161918720.jpg)', backgroundSize: 'cover', minHeight: '275px', padding: '20px', color: '#1B1B1B' }}>
