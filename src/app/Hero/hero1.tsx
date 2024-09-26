5c8805ae77fba1cd2c260567051ed9620abcbaf2
ramya72002 <nsriramya7@gmail.com>
2024-09-26 10:37:36 +0530

    level1 vedio added

--- src/app/Hero/hero1.tsx
+++ src/app/Hero/hero1.tsx
@@ -1,36 +1,53 @@
 import React from 'react';
-import './hero1.scss';
+import './hero1.scss'; // Using the import style as requested
 
-const ChessComponent = () => {
-    return (
-        <section className="hero">
-            <div style={{ display: 'flex', padding: '7em 0' }}>
-                <div style={{ width: '40%', padding: '3em 5em', display: 'flex', flexDirection: 'column', gap: '1em' }}>
-                <h6>WHO WE ARE</h6>
-                <h2>The magic of chess, beauty and complexity.</h2>
-                    <div className="chess-section">
-                        <img src="/images/chessclub.png" alt="Chess Club" className="chess-image" />
-                        <div>
-                            <h4>Chess Club</h4>
-                            <p>Dignissim mus orci pretium...</p>
-                        </div>
-                    </div>
-                    <div className="chess-section">
-                        <img src="/images/chesstrain.png" alt="Chess Training Center" className="chess-image" />
-                        <div>
-                            <h4>Chess Training Center</h4>
-                            <p>Dignissim mus orci pretium...</p>
-                        </div>
-                    </div>
-                </div>
-                <div style={{ width: '50%', backgroundImage: `url(http://kit.creativemox.com/gambit/wp-content/uploads/sites/10/2023/10/chessboard-with-black-chess-figures-isolated-on-black-e1698059185527.jpg)`, backgroundSize: 'cover' }}>
-                    <h4>Full of passion and dedication...</h4>
-                    <img src="http://kit.creativemox.com/gambit/wp-content/uploads/sites/10/2023/10/Martin-Richter-marking.png" alt="GM Martin Richter" />
-                    <h6>GM MARTIN RICHTER</h6>
-                </div>
+const Hero1 = () => {
+  return (
+    <div className="hero1">
+      <div className="container">
+        {/* Left section */}
+        <div className="content-left">
+          <h6 className="who-we-are">WHO WE ARE</h6>
+          <h1 className="main-title">The magic of chess, beauty and complexity.</h1>
+          <p className="description">
+            Dignissim mus orci pretium leo nascetur nam aliquet metus feugiat faucibus luctus.
+          </p>
+          
+          <div className="icon-box">
+            <i className="mdi mdi-chess-king"></i>
+            <div className="icon-content">
+              <h4>Chess Club</h4>
+              <p>Dignissim mus orci pretium leo nascetur nam aliquet metus feugiat faucibus luctus</p>
             </div>
-        </section>
-    );
+          </div>
+
+          <div className="icon-box">
+            <i className="fas fa-chess"></i>
+            <div className="icon-content">
+              <h4>Chess Training Center</h4>
+              <p>Dignissim mus orci pretium leo nascetur nam aliquet metus feugiat faucibus luctus</p>
+            </div>
+          </div>
+        </div>
+
+        {/* Right section with background image */}
+        <div className="content-right">
+          <div className="image-overlay">
+            <div className="image-section">
+              <h4 className="passion-dedication">
+                Full of passion and dedication. Welcome to a chess school that is always ready to help you improve
+              </h4>
+              <img src="http://kit.creativemox.com/gambit/wp-content/uploads/sites/10/2023/10/Martin-Richter-marking.png" alt="GM Martin Richter" />
+              <h6 className="gm-name">GM MARTIN RICHTER</h6>
+            </div>
+            <div className="image-container">
+              <img src="http://kit.creativemox.com/gambit/wp-content/uploads/sites/10/2023/10/portrait-of-a-confident-businessman-in-a-modern-factory4-e1698115669998.png" alt="Portrait" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default ChessComponent;
+export default Hero1;
