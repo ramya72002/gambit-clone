f8a663f53d8b0cacba221c25b88259cbae3059e1
ramya72002 <nsriramya7@gmail.com>
2024-09-26 11:38:40 +0530

    level1 vedio added

--- src/app/Hero/hero4/page.tsx
+++ src/app/Hero/hero4/page.tsx
@@ -1,37 +1,48 @@
-// components/Hero4.js
 import React from 'react';
-import './hero4.scss'; // Importing the SCSS file for styles
+import './hero4.scss';
 
 const Hero4 = () => {
   return (
-    <div className="hero-container">
-      <div className="text-container">
-        <h6 className="heading">OUR Class</h6>
-        <h2 className="subheading">Creating a winning mentality, giving birth to a tough generation.</h2>
-        <p className="description">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        </p>
-        <div className="icon-box">
-          <div className="icon">
-            <i className="fas fa-globe"></i>
+    <div className="hero4-container">
+      <div className="hero4-content">
+        <div className="hero4-left">
+          <h6 className="hero4-title">OUR Class</h6>
+          <h2 className="hero4-subtitle">
+            Creating a winning mentality, giving birth to a tough generation
+          </h2>
+          <div className="hero4-text">
+            {/* Additional text content can be added here */}
           </div>
-          <h4>Online Class</h4>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
-        </div>
-        <div className="icon-box">
-          <div className="icon">
-            <i className="fas fa-chess"></i>
+          <div className="hero4-icon-box">
+            <div className="icon-box">
+              <i className="fas fa-globe icon" />
+              <div className="icon-box-content">
+                <div className="icon-box-title">Online Class</div>
+                <div className="icon-box-description">
+                  Engaging online classes designed for effective learning.
+                </div>
+              </div>
+            </div>
+            <div className="icon-box">
+              <i className="fas fa-chess icon" />
+              <div className="icon-box-content">
+                <div className="icon-box-title">Regular Class</div>
+                <div className="icon-box-description">
+                  In-person classes that foster interaction and collaboration.
+                </div>
+              </div>
+            </div>
           </div>
-          <h4>Regular Class</h4>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
         </div>
-      </div>
-      <div className="image-container">
-        <img
-          src="http://kit.creativemox.com/gambit/wp-content/uploads/sites/10/2023/10/chess-playing-e1697916016480.jpg"
-          alt="Chess Playing"
-          className="hero-image"
-        />
+        <div className="hero4-right">
+          <div className="image-mask-container">
+            <img
+              src="http://kit.creativemox.com/gambit/wp-content/uploads/sites/10/2023/10/chess-playing-e1697916016480.jpg"
+              alt="Chess Playing"
+              className="hero4-image"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
